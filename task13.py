fe121e027f72c783b9c159c87bf9fb1f86564235
d1on1s1y <dionisiy.god.of.wine@gmail.com>
2023-10-17 23:48:38 +0300

    Update task13.py

--- task13.py
+++ task13.py
@@ -1,6 +1,6 @@
 import numpy as np
 list1 = ['халупа', 'сонце', 'кіт', 'молоко', 'річка']
 list2 = ['молоко', 'яблуко', 'сонце', 'гора', 'дім', 'книга', 'кіт']
-def same_elements(list1,list2):
-    return np.intersect1d(list1, list2)
-print (same_elements(list1,list2))
+def notsame_elements(list1,list2):
+    return np.setxor1d(list1, list2)
+print (notsame_elements(list1,list2))
