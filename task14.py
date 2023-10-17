22a9908b4c86f935d4a494c1613bd1f5425432c1
d1on1s1y <dionisiy.god.of.wine@gmail.com>
2023-10-17 23:51:47 +0300

    Update task14.py

--- task14.py
+++ task14.py
@@ -3,7 +3,6 @@ summer_months = ('червень', 'липень', 'серпень')
 autumn_months = ('вересень', 'жовтень', 'листопад')
 winter_months = ('грудень', 'січень', 'лютий')
 
-# Создание кортежа, в котором месяцы сгруппированы по временам года
 seasons = (spring_months, summer_months, autumn_months, winter_months)
 
 print(seasons)
